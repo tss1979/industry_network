6896893fb29f0cb2b76ce43641a01e8d569189f3
sergeytashkinov <tashkinov2@gmail.com>
2025-02-12 09:59:15 +0300

    admin finished

--- network/admin.py
+++ network/admin.py
@@ -10,15 +10,15 @@ def reset_credit(self, request, queryset):
 # Register your models here.
 @admin.register(Plant)
 class PlantAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'supplier')
-    list_filter = ('name',)
-    search_fields = ('name',)
+    list_display = ('id', 'name', 'supplier', 'credit',)
+    list_filter = ('name', 'contacts__city',)
+    search_fields = ('name', 'contacts__city',)
     actions = [reset_credit]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name',)
     list_filter = ('name',)
     search_fields = ('name',)
     actions = [reset_credit]
@@ -26,22 +26,22 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Entrepreneur)
 class EntrepreneurAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'supplier')
-    list_filter = ('name',)
-    search_fields = ('name',)
+    list_display = ('id', 'name', 'supplier', 'credit',)
+    list_filter = ('name', 'contacts__city',)
+    search_fields = ('name', 'city', 'contacts__city',)
     actions = [reset_credit]
 
 
 @admin.register(Retail)
 class RetailAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'supplier', 'credit')
-    list_filter = ('name',)
-    search_fields = ('name',)
+    list_display = ('id', 'name', 'supplier', 'credit',)
+    list_filter = ('name', 'contacts__city',)
+    search_fields = ('name', 'contacts__city',)
     actions = [reset_credit]
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email')
-    list_filter = ('email',)
-    search_fields = ('email',)
+    list_display = ('id', 'email',)
+    list_filter = ('email', 'city',)
+    search_fields = ('email', 'city',)
