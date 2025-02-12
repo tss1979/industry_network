698e219347a9c6869f8411fab51ee78fbc07ff43
sergeytashkinov <tashkinov2@gmail.com>
2025-02-12 09:46:27 +0300

    permitions add

--- network/views.py
+++ network/views.py
@@ -1,38 +1,23 @@
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from network.serializers import PlantSerializer, RetailSerializer, EntrepreneurSerializer
 from network.models import Plant, Retail, Entrepreneur
-from network.permissions import UserIsAdminPermission
-
-# Create your views here.
+from network.permissions import UserIsActivePermission
 
 
-class LevelElementViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated, UserIsAdminPermission]
-
-    @action(detail=True, methods=("PATCH", ))
-    def reset_credit(self, pk):
-        element = get_object_or_404(self.queryset, pk)
-        if element:
-            element.credit = 0
-            element.save()
-        serializer = self.get_serializer(element)
-        return Response(data=serializer.data)
-
-
-class PlantViewSet(LevelElementViewSet):
+class PlantViewSet(viewsets.ModelViewSet):
     serializer_class = PlantSerializer
     queryset = Plant.objects.all()
+    permission_classes = [IsAuthenticated, UserIsActivePermission]
 
 
-class RetailViewSet(LevelElementViewSet):
+class RetailViewSet(viewsets.ModelViewSet):
     serializer_class = RetailSerializer
     queryset = Retail.objects.all()
+    permission_classes = [IsAuthenticated, UserIsActivePermission]
 
 
-class EntrepreneurViewSet(LevelElementViewSet):
+class EntrepreneurViewSet(viewsets.ModelViewSet):
     serializer_class = EntrepreneurSerializer
     queryset = Entrepreneur.objects.all()
+    permission_classes = [IsAuthenticated, UserIsActivePermission]
